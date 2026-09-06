2797effda68bf4c911de4a6228cb666ffc3f5ade
unknown <NGHANYI SALAMATOU WOUYUEKE>
2026-09-06 19:21:54 +0100

    add vercel rewrite config for client-side routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,22 @@ import ContactPage from "./pages/ContactPage";
 import BlogPage from "./pages/BlogPage";
 import BlogPostPage from "./pages/BlogPostPage";
 
+const orgData = {
+
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Nomad Coffee Roasters",
+  "url": "https://nomad-coffee-roaster.vercel.app",
+}
+
 export default function App() {
   return (
     <>
       <Navbar />
+      <script
+       type="application/ld+json"
+       dangerouslySetInnerHTML={{ __html: JSON.stringify(orgData)
+       }}/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
